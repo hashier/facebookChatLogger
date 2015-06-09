4c65547f42977a7570d74040a69e1be7fc519cad
Christopher Loessl <christopher.loessl@truecaller.com>
2015-06-09 15:57:26 +0200

    Adds: Some test code for Ono and hpple

---
+++ sortFacebookMessenger/Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,20 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// Ono
+#define COCOAPODS_POD_AVAILABLE_Ono
+#define COCOAPODS_VERSION_MAJOR_Ono 1
+#define COCOAPODS_VERSION_MINOR_Ono 2
+#define COCOAPODS_VERSION_PATCH_Ono 2
+
+// hpple
+#define COCOAPODS_POD_AVAILABLE_hpple
+#define COCOAPODS_VERSION_MAJOR_hpple 0
+#define COCOAPODS_VERSION_MINOR_hpple 2
+#define COCOAPODS_VERSION_PATCH_hpple 0
+
--- sortFacebookMessenger/sortFacebookMessenger/main.m
+++ sortFacebookMessenger/sortFacebookMessenger/main.m
@@ -8,10 +8,115 @@
 
 #import <Foundation/Foundation.h>
 
+#import "Ono.h"
+
+#import "TFHpple.h"
+
+@implementation NSString (Extensions)
+- (NSString *)trim {
+    return [self stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+}
+@end
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        NSError *error;
+        NSString *xPath = @"//div[@class='thread']";
+        NSData *file;
+        __block int i;
+        i = 0;
+        __block NSString *a = @"", *b = @"";
+        ONOXMLDocument *document;
+        
+        file = [NSData dataWithContentsOfFile:@"/Users/chl/Downloads/facebook-christopherloessl/html/messages.htm"];
+//        file = [NSData dataWithContentsOfFile:@"/Users/chl/Desktop/test.html"];
+//        file = [NSData dataWithContentsOfFile:@"/Users/chl/Desktop/test2.html"];
+        file = [NSData dataWithContentsOfFile:@"/Users/chl/Desktop/test3.html"];
+        
+        
+        document = [ONOXMLDocument XMLDocumentWithData:file error:&error];
+        
+
+        for (ONOXMLElement *element in document.rootElement.children) {
+            i++;
+            NSLog(@"1: %@: %@", element.tag, element.attributes);
+        }
+//        NSLog(@"End: %d", i);
+        
+        ONOXMLElement *tmp = [document.rootElement firstChildWithTag:@"thread"];
+        i = 0;
+        for (ONOXMLElement *element in tmp.children) {
+            i++;
+//            NSLog(@"%@: %@", element.tag, element.attributes);
+        }
+//        NSLog(@"End: %d", i);
+        
+        __block NSMutableString *tmpString = [NSMutableString new];
+        NSLog(@"hashier: time 0");
+//        NSLog(@"XPath Search: %@", XPath);
+        [document enumerateElementsWithXPath:xPath usingBlock:^(ONOXMLElement *element, NSUInteger idx, BOOL *stop) {
+//            NSLog(@"%@", [element stringValue]);
+            [tmpString appendString:[element stringValue]];
+            //*stop = idx < 3;
+            i++;
+        }];
+        NSLog(@"entries: = %d and length = %lu", i, tmpString.length);
+        
+        [document enumerateElementsWithXPath:xPath usingBlock:^(ONOXMLElement *element, NSUInteger idx, BOOL *stop) {
+            NSLog(@"\n%@", [element stringValue]);
+            a = [a stringByAppendingString:[element stringValue]];
+        }];
+        
+        NSLog(@"hashier: time 1");
+        TFHpple *tutorialsParser = [TFHpple hppleWithHTMLData:file];
+        NSLog(@"hashier: time 2");
+        NSArray *tutorialsNodes = [tutorialsParser searchWithXPathQuery:xPath];
+        NSLog(@"hashier: time 3");
+        
+        for (TFHppleElement *element in tutorialsNodes) {
+//            NSLog(@"%@", [[element firstChild] content].trim);
+        }
+        NSLog(@"elements: %lu", tutorialsNodes.count);
+        
+        
+        putchar('\n');
+        putchar('\n');
+        putchar('\n');
+        putchar('\n');
+        putchar('\n');
+
+        
+        file = [NSData dataWithContentsOfFile:@"/Users/chl/Desktop/test3.html"];
+        
+        NSLog(@"XPathQuery: %@", xPath);
+        
+        
+        NSLog(@"\n\n\nhpple\n\n\n");
+
+        tutorialsParser = [TFHpple hppleWithHTMLData:file];
+        tutorialsNodes = [tutorialsParser searchWithXPathQuery:xPath];
+        
+        for (TFHppleElement *element in tutorialsNodes) {
+            NSLog(@"%@", [[element firstChild] content].trim);
+        }
+        
+        
+        NSLog(@"\n\n\nOno\n\n\n");
+        
+        document = [ONOXMLDocument HTMLDocumentWithData:file error:&error];
+        
+        [document enumerateElementsWithXPath:xPath usingBlock:^(ONOXMLElement *element, NSUInteger idx, BOOL *stop) {
+            NSLog(@"\n%@", [element stringValue]);
+            b = [b stringByAppendingString:[element stringValue]];
+        }];
+        
+        if ([a isEqualToString:b])
+            NSLog(@"same");
+        else
+            NSLog(@"not same");
+        
+        
+        
     }
     return 0;
 }
